48b196f7be07476a9a3eafd508db1dd42f864cf3
inaijhs <74161585+Inaijhs95@users.noreply.github.com>
2024-11-22 09:14:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+console.log("Auth initialized:", auth); // 確認用ログ
 
 // reCAPTCHAの設定
 const recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
